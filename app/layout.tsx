472752e64cd41350c4db46b35574f7bf4322ad31
Nikita <nikita.shalashkov@gmail.com>
2023-04-21 19:59:12 +0300

    Modals

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,33 @@
-import { Nunito } from 'next/font/google'
+import { Nunito } from "next/font/google";
 
-import './globals.css'
-import Navbar from './components/navbar/Navbar'
+import "./globals.css";
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import Modal from "./components/modals/Modal";
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'This is Airbnb clone',
-}
+  title: "Airbnb",
+  description: "This is Airbnb clone",
+};
 
 const font = Nunito({
-  subsets: ['latin'],
-})
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          <Modal isOpen />
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
-  )
+  );
 }
